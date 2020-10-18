2d5b4cb5c18658ab9761d07c9cf9e4f0c2238154
roedor-desenvolvedor <phi.alencart@gmail.com>
2020-10-18 18:38:54 -0300

    added nodemon

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.log('oi meninas turubom')
+//importar pacote express
+const express = require('express');
+//require exige que o pacote express esteja instalado
+
+//iniciando express
+const server = express()
+
+//criar rota para servidor
+server.get('/', (request, response)=> {
+   return response.send('Hello from the server siiiiiide')
+})
+
+//ligar servidor
+server.listen(5500)
